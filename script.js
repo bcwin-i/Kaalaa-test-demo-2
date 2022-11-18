c5a52d3e9129cb2cbbf5933f6d9a64ef764f5d40
“bcwin-i” <iziqbek@gmail.com>
2022-11-18 15:02:42 +0000

    Position

--- script.js
+++ script.js
@@ -454,7 +454,7 @@ async function generateQRCode(data) {
 
 async function createDownload() {
   const newdiv = document.createElement("div");
-  newdiv.id = "kaalaa_QRContainer";
+  newdiv.id = "QRContainer";
   newdiv.style.padding = "20px";
   document.body.appendChild(newdiv);
 
@@ -584,16 +584,31 @@ document.addEventListener("mouseout", (e) => {
   }
 });
 
+var timer_containers = document.getElementsByClassName("kaalaa_timer_container");
+
+var timerClickHandler = function() {
+  e.preventDefault();
+    e.stopPropagation();
+  const itemId = this.dataset.timer;
+  const rewardClaim = this.dataset.reward;
+  if (itemId && rewardClaim) {
+    current_reward = { itemId, amount: 1 };
+    modalDisplay();
+    return;
+  }
+};
 
 for (var i = 0; i < timer_containerslength; i++) {
-  timer_containers[i].addEventListener('click', myFunction, false);
+  timer_containers[i].addEventListener('click', timerClickHandler, false);
 }
 
 document.addEventListener("click", async (e) => {
+  // console.log("Class: ", e.target.className)
+  if (e.target.className === "kaalaa_timer_container") {
+    e.preventDefault();
+    e.stopPropagation();
+  }
   const id = e.target.id;
-
-  const itemId = e.target.dataset.timer;
-  const rewardClaim = e.target.dataset.reward;
 
   if (!e.target.href || e.target.href === "") e.preventDefault();
 
@@ -640,15 +655,17 @@ document.addEventListener("click", async (e) => {
     return;
   }
 
-  if (itemId && rewardClaim) {
-    e.preventDefault();
-    current_reward = { itemId, amount: 1 };
-    modalDisplay();
-    return;
-  }
+  // if (itemId && rewardClaim) {
+  //   e.preventDefault();
+  //   e.stopPropagation();
+  //   current_reward = { itemId, amount: 1 };
+  //   modalDisplay();
+  //   return;
+  // }
 
   if (id === "claim_reward_button") {
     e.preventDefault();
+    e.stopPropagation();
     const modalStatus = getElementById("modalStatusContainer");
     if (modalStatus) modalStatus.innerHTML = request_loader;
 
@@ -661,7 +678,9 @@ document.addEventListener("click", async (e) => {
     if (timer && req.status) {
       if (req.status) {
         globalClaim = true;
-        const timers = document.getElementsByClassName("timer_container");
+        const timers = document.getElementsByClassName(
+          "kaalaa_timer_container"
+        );
         if (timers.length > 0) {
           setTimeout(() => {
             for (var i = 0; i < timers.length; i++) {
